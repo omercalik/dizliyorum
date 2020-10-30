8b0bec135ea08f69e71d1ac063d48e38c9c4d6c0
Yalçın Golayoğlu <malozyali@gmail.com>
2020-10-30 19:55:33 +0300

    Film Api eklendi.

---
+++ src/components/dashboard/Anasayfa.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Anasayfa() {
+    return (
+        <div>
+            <h1>KULLANICI GİRİŞİ YAPILDIKTAN SONRA SİTE SAYFASI AÇILMALI.</h1>
+        </div>
+    )
+}
