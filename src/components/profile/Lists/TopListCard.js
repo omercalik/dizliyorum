b2911d08b93fe146a81bd1a4a24f2e71b030754e
Yalçın Golayoğlu <malozyali@gmail.com>
2020-11-05 21:45:38 +0300

    init

--- src/components/profile/Lists/TopListCard.js
+++ src/components/profile/Lists/TopListCard.js
@@ -2,10 +2,11 @@ import React from 'react';
 import '../../../Results.css';
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
-
+    
  const Vizyondakiler = ({ title, poster_path, overview, vote_average }) => (
     
         <div className = "movieTopList">
+            
             <img src = {IMG_API + poster_path} alt = {title}/>
             <div className = "movieTopList-info">
             <h3>{title}</h3>
